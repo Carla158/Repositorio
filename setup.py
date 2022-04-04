fd29c6f1af2a77c9948ba312e768d94af52ab00a
Sil Fer <carlariverofernandez@gmail.com>
2022-04-04 20:18:10 -0300

    primer commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+    name = "PaqueteParientes", 
+    version="1.0",
+    description="entregable MTV",
+    author="Car",
+    author_email="carlariverofernandez@gmail.com",
+    packages=["PaqueteParientes"]
+)
+#py setup.py sdist (para que genere el .gz que es el que se comparte)
